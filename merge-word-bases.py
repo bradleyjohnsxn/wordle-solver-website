f87a5d49c436f517778958e4baeae3b8879f5652
bradleyjohnsxn <62025141+bradleyjohnsxn@users.noreply.github.com>
2023-04-05 21:29:38 -0400

    implemented wordle solver in python and increased size of word bank

---
+++ merge-word-bases.py
@@ -0,0 +1,33 @@
+import numpy as np
+import pandas as pd
+
+# load 5 letter words from file
+def load_words(filepath):
+    word_file = open(filepath, 'r')
+    words = word_file.readlines()
+    for i in range(len(words)): words[i]=words[i][:5]
+    word_file.close()
+    return words
+
+# load map of words and their frequency
+def load_word_freqs():
+    filepath = 'data/5letter_unigram_freq.csv'
+    return dict(pd.read_csv(filepath).to_numpy())
+
+
+if __name__=='__main__':
+    words_txt1 = load_words('data/5letterwords.txt')
+    words_txt2 = load_words('data/5letterwords2.txt')
+    words_csv = np.array(list(load_word_freqs().keys()))
+    words_merged = np.array(list(set(list(np.append(np.append(words_txt1, words_txt2), words_csv)))))
+
+    print(f'words in original text file 1: {len(words_txt1)}')
+    print(f'words in original text file 2: {len(words_txt2)}')
+    print(f'words in original csv file   : {len(words_csv)}')
+    print(f'word count total             : {len(words_merged)}')
+
+    filepath = 'data/5letterwords-updated'
+    file = open(filepath, 'w')
+    for word in words_merged:
+        file.write(word+'\n')
+    file.close()
--- wordle-solver.py
+++ wordle-solver.py
@@ -24,7 +24,110 @@ class  wordle_solver():
         filepath = 'data/5letter_unigram_freq.csv'
         self.freqs =  dict(pd.read_csv(filepath).to_numpy())
 
+    # add green
+    def add_green(self, green): 
+        self.greens=np.append(self.greens, str(green))
+        self.green_added()
+    
+    # add yellow
+    def add_yellow(self, yellow): 
+        self.yellows=np.append(self.yellows, str(yellow))
+        self.yellow_added()
+
+    # add used
+    def add_used(self, u): 
+        for i in range(len(str(u))): self.used=np.append(self.used, str(u)[i])
+        self.used_added(len(u))
+
+    # get index/letter map
+    def ind_map(self, s):
+        inds = dict()
+        for i in range(5): 
+            if s[i].isalpha(): inds[i] = s[i]
+        return inds
+
+    # adjust possible after green added
+    def green_added(self):
+        inds = self.ind_map(self.greens[len(self.greens)-1])
+
+        res = []
+        for i in range(len(self.possible)):
+            for (ind, let) in inds.items():
+                if self.possible[i][ind] != let: res += [i]; break
+
+        res = np.sort(res)[::-1]
+        for i in res: self.possible.remove(self.possible[i])
+
+    # adjust possible after yellow added
+    def yellow_added(self):
+        inds = self.ind_map(self.yellows[len(self.yellows)-1])
+
+        res = []
+        for i in range(len(self.possible)):
+            for (ind, let) in inds.items():
+                if self.possible[i][ind] == let: res += [i]; break
+                b=False
+                for j in range(5):
+                    if j!=ind and self.possible[i][j] == let: break
+                    elif (j==4 and ind!=4) or (j==3 and ind==4): res += [i]; b=True
+                if b: break
+
+        res = np.sort(res)[::-1]
+        for i in res: self.possible.remove(self.possible[i])
+
+    # adjust possible after letters used
+    def used_added(self,  n):
+        u = self.used[-n:]
+        needed = []
+        for i in range(len(self.yellows)):
+            for let in self.ind_map(self.yellows[i]).values():
+                needed += [let]
+        for i in range(len(self.greens)):
+            for let in self.ind_map(self.greens[i]).values():
+                needed += [let]
+        for x in needed:
+            for y in u:
+                if x==y: u = np.delete(u, np.where(u == y))
+
+        res = []
+        for i in range(len(self.possible)):
+            for j in range(len(u)):
+                if u[j] in self.possible[i]: res += [i]; break
+
+        res = np.sort(res)[::-1]
+        for i in res: self.possible.remove(self.possible[i])
+
+    # sort and print possible words
+    def print_possible(self):
+        stacked = dict()
+        for word in self.possible:
+            try: stacked[word] = int(self.freqs[word])
+            except KeyError as ke: stacked[word] = 0   
+
+        stacked = {k: v for k, v in np.array(sorted(stacked.items(), key=lambda item: item[1]))[::-1]}
+        print(f'\n{len(ws.possible)} possible word(s):')
+        for num, word in enumerate(stacked.keys(), 1): 
+            if num < 11: print(f'{num}. {word}')
+            else: break
 
 
 if __name__=='__main__':
-    ws = wordle_solver()
+    go = True
+    while go:
+        ws = wordle_solver()
+        cont = True
+        while cont:
+            g = input('\nenter greens: ')
+            ws.add_green(g)
+            y_done = False
+            while not y_done:
+                y = input('enter yellows (type "end" to continue): ')
+                if y=='end': y_done = True
+                else: ws.add_yellow(y)
+            u = input('enter used letters: ')
+            ws.add_used(u)
+            ws.print_possible()
+            c = input('\ncontinue? (y/n) ')
+            if c != 'y': cont=False
+        aw = input('another word? (y/n)')
+        if aw != 'y': go = False        
