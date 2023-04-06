d3ca9ad8ae0cfc9f700bee6c78b0b3c21d382da5
bradleyjohnsxn <62025141+bradleyjohnsxn@users.noreply.github.com>
2023-04-06 18:16:26 -0400

    made crud app using flask

--- app.py
+++ app.py
@@ -1,16 +1,57 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-@app.route('/',methods = ['GET'])
+class datab(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    data_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
+
+@app.route('/', methods=['POST', 'GET'])
 def show_index_html():
-    return render_template('wordle-solver.html')
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = datab(content=task_content)
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "there was an issue adding your task"
+    else:
+        tasks = datab.query.order_by(datab.data_created).all()
+        return render_template('wordle-solver.html', tasks=tasks)
+    
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = datab.query.get_or_404(id)
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'there was a problem deleting that task'
 
-@app.route('/send_data', methods = ['POST'])
-def get_data_from_html():
-        pay = request.form['pay']
-        print ("Pay is " + pay)
-        return "Data sent. Please check your program log"
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    task = datab.query.get_or_404(id)
+    if request.method=='POST':
+        task.content = request.form['content']
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'there was a problem updating that task'
+    else:
+        return render_template('update.html', task=task)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(debug=True)
