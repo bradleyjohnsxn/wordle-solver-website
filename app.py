fc74e830e6de9fa437d6a0de7cddec126c9c4149
bradleyjohnsxn <62025141+bradleyjohnsxn@users.noreply.github.com>
2023-04-07 10:45:12 -0400

    fresh db for wordle solver

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from datetime import datetime
 import wordle_solver
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///wordle_solver.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 wordle_db = SQLAlchemy(app)
 ws = wordle_solver.wordle_solver()
